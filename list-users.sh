52936e924e4c50eff8e3cf5308600b2072d9ace3
Shifa Khan <67899867+shifa-khan@users.noreply.github.com>
2024-04-28 20:26:51 -0400

    list-users.sh

---
+++ list-users.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+###########
+# About: This script is used for fetching usernames of members of a GitHub organization using an API call
+# Input parameters:
+# Before running the script export the username and token
+# export="username"
+# export="token"
+###########
+
+# calling help function
+help()
+
+# GitHub API URL
+API_URL="https://api.github.com"
+
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+# Function to make a GET request to the GitHub API
+function github_api_get {
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions.pull == true) | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+function help {
+  cmd-args = 2
+  if [ $# -ne $cmd-args ]; then
+  echo "Please re-check the cmd line args "
+  echo " format of the cmd ars -> ./filename org-name repo-name"
+}
+
+# Main script
+
+echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
